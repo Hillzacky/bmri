2c19b7753a4f6763e162f41aaf98350022ff6dcc
Hilmansyah <hilm4nsyah@gmail.com>
2024-02-11 01:43:04 +0700

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,219 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'hillzacky/bmri',
+  ),
+  'versions' => 
+  array (
+    'guzzle/batch' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/cache' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/common' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/guzzle' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '3.9.x-dev',
+      ),
+      'reference' => 'f7778ed85e3db90009d79725afd6c3a82dab32fe',
+    ),
+    'guzzle/http' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/inflection' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/iterator' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/log' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/parser' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-async' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-backoff' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-cache' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-cookie' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-curlauth' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-error-response' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-history' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-log' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-md5' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-mock' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/plugin-oauth' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/service' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'guzzle/stream' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'dev-master',
+        1 => '3.9.x-dev',
+      ),
+    ),
+    'hillzacky/bmri' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/event-dispatcher' => 
+    array (
+      'pretty_version' => '2.8.x-dev',
+      'version' => '2.8.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a77e974a5fecb4398833b0709210e3d5e334ffb0',
+    ),
+  ),
+);
